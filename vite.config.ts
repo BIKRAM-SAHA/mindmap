daa11faa37be0bf5663ede84d0ee23ac86d90dd8
BIKRAM-SAHA <bikramlearn15@gmail.com>
2025-04-23 01:07:00 +0530

    add: docker setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,18 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 const config = defineConfig({
+    base: "/",
     plugins: [react()],
+    preview: {
+        port: 8080,
+        strictPort: true,
+    },
+    server: {
+        port: 8080,
+        strictPort: true,
+        host: true,
+        origin: "http://0.0.0.0:8080",
+    },
     resolve: {
         alias: [
             {
